98b2a1c71b7c05b860094b7de72fbdbabb01706f
Mela Ford <mela.ford02@gmail.com>
2025-09-12 19:06:17 -0700

    fixed efficiency errors

--- split.cpp
+++ split.cpp
@@ -11,44 +11,48 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef> // for NULL
 
 /* Add a prototype for a helper function here if you need */
-
-static void addNode(Node*& head, Node* curr);
+static void addToEnd(Node*& list, Node* item);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  // WRITE YOUR CODE HERE
 
-if (in == nullptr){
+  if(in == nullptr) {
     return;
-}
+  }
 
-Node* curr = in; 
-Node* rest = in->next;
-curr->next = nullptr;
+  Node* cur = in;
+ 
+  if (in != nullptr){
+    in = in->next;
+    cur->next = nullptr;
+  }
 
-if (curr->value % 2 == 0){
-    addNode(evens, curr);
-} else{
-    addNode(odds, curr);
-}
+  // decide if odd or even
+  int num_value = cur->value;
+  bool even_num = (num_value % 2 == 0);
 
-in = rest;
-split(in, odds, evens);
+  if (even_num){
+    addToEnd(evens, cur);
+  } else {
+    addToEnd(odds, cur);
+  }
 
+  
+  split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
-static void addNode(Node*& head, Node* curr){
-    if (head == nullptr){
-        head = curr;
-        return;
-    }
-    if(head->next == nullptr){
-        head->next = curr;
-        return;
-    }
-    addNode(head->next, curr);
+static void addToEnd(Node*& list, Node* item) {
+if (list == nullptr){
+    list = item;
+} else if (list->next == nullptr){
+    list->next = item;
+} else {
+    addToEnd(list->next, item);
+}
 }
